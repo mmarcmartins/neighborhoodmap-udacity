a976c5c091043762e3dc8c2fb2a6a33875079eaa
marceloLinux <mmarcmartins@gmail.com>
2019-05-08 19:41:27 -0300

    marker adicionado

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import Map from "../components/map/Map";
 import { LoadScript } from "@react-google-maps/api";
-import { getAddress, defaultMarkers, filterResult } from "../utils/map";
+import { getAddress, defaultMarkers } from "../utils/map";
 import Locations from "../components/locations/Locations";
 import ActionsMenu from "../components/actionsmenu/ActionsMenu";
 import "../utils/fonts";
 import "../main.scss";
+import ModalLocations from "../components/modalLocations/ModalLocations";
 
 class Mainpage extends Component {
   state = {
     markers: defaultMarkers,
-    foundMarkers: [],
     noLocations: false
   };
 
   addMarker = location => {
     getAddress(google, location).then(result => {
       if (result.status === google.maps.GeocoderStatus.OK) {
-        this.setFoundMarkers(result);
+        this.insertMarker(result.results[0]);
         this.changeLocationStatus();
       } else {
         this.changeLocationStatus();
@@ -25,11 +25,26 @@ class Mainpage extends Component {
     });
   };
 
+  insertMarker = marker => {
+    const newMarker = {
+      name: marker.formatted_address,
+      coords: {
+        lat: marker.geometry.location.lat(),
+        lng: marker.geometry.location.lng(),
+      }
+    }
+    this.setState(state => ({
+      markers: state.markers.concat([newMarker])
+    }));
+  };
+
+
   changeLocationStatus = () => {
     this.setState(state => ({ noLocation: !state.noLocation }));
   };
 
-  filterList = () => {};
+
+  filterList = () => { };
 
   render() {
     const { markers } = this.state;
